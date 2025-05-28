35bc412d732ee071e18603eac250b424df59f219
Martin <martin18.ms38@gmail.com>
2025-05-27 22:29:15 -0300

    Eliminados archivos duplicados por conflicto de merge con main

--- NutriCheck.Backend/Dtos/LoginUserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NutriCheck.Backend.Dtos
-{
-    public class LoginUserDto
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
